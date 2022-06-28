2da49f6c1b81133df8dde040db9528ec1ede5de9
Fernando Rios <feriosch98@gmail.com>
2022-06-28 10:34:36 -0500

    Collection schema refactor

--- jap_dev/views/collection/main.py
+++ jap_dev/views/collection/main.py
@@ -3,8 +3,8 @@ from flask import make_response
 
 from jap_dev.helpers.authentication import validate_session
 from jap_dev.schemas.validation import validate_schema, get_params
-from jap_dev.schemas.collections.main.get import collection_get_schema
-from jap_dev.schemas.collections.main.insert import collection_insert_schema
+from jap_dev.schemas.collection.main.get import collection_get_schema
+from jap_dev.schemas.collection.main.insert import collection_insert_schema
 from jap_dev.responses.collection.main.get import get_collections_response
 from jap_dev.responses.collection.main.insert import insert_collection_response
 
