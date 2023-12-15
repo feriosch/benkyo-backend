44bb38760d6eff7d4224c30f7f50e1b13adaae5f
Fernando Rios <feriosch98@gmai.com>
2023-12-15 16:45:55 +0900

    Changed word tag schema

--- jap_dev/schemas/word/main/insert.py
+++ jap_dev/schemas/word/main/insert.py
@@ -13,20 +13,6 @@ word_type_insert_schema = Schema({
     Optional('counter'): And(Use(int), lambda x: 0 < x <= 9, error='type.counter error'),
 })
 
-word_tags_insert_schema = Schema({
-    Optional('ateji'): Use(bool, error='tags.ateji error'),
-    Optional('common'): Use(bool, error='tags.common error'),
-    Optional('expression'): Use(bool, error='tags.expression error'),
-    Optional('honorific'): Use(bool, error='tags.honorific error'),
-    Optional('humble'): Use(bool, error='tags.humble error'),
-    Optional('intransitive'): Use(bool, error='tags.intransitive error'),
-    Optional('jlpt_n1'): Use(bool, error='tags.jlpt_n1 error'),
-    Optional('joyogai'): Use(bool, error='tags.joyogai error'),
-    Optional('onomatopoeic'): Use(bool, error='tags.onomatopoeic error'),
-    Optional('transitive'): Use(bool, error='tags.transitive error'),
-    Optional('usually_kana'): Use(bool, error='tags.usually_kana error'),
-})
-
 word_insert_schema = Schema({
     'word': Use(str, error='word error'),
     'spanish': Use(str, error='spanish error'),
@@ -34,7 +20,7 @@ word_insert_schema = Schema({
     Optional('hiragana'): Use(str, error='hiragana error'),
     Optional('level'): And(Use(int), lambda x: 0 <= x <= 5, error='level error'),
     Optional('word_type'): word_type_insert_schema,
-    Optional('tags'): word_tags_insert_schema,
+    Optional('tags'): Schema([Use(str, error='tag error')]),
     Optional('sentences'): Schema([{
         'sentence': Use(str, error='sentences.sentence error'),
         'translation': Use(str, error='sentences.translation error')
--- jap_dev/schemas/word/main/update.py
+++ jap_dev/schemas/word/main/update.py
@@ -1,6 +1,6 @@
 from schema import Schema, Optional, Use
 
-from jap_dev.schemas.word.main.insert import word_type_insert_schema, word_tags_insert_schema
+from jap_dev.schemas.word.main.insert import word_type_insert_schema
 
 word_update_schema = Schema({
     'word_id': Use(str, error='id error (required)'),
@@ -8,7 +8,7 @@ word_update_schema = Schema({
     Optional('spanish'): Use(str, error='spanish error'),
     Optional('hiragana'): Use(str, error='hiragana error'),
     Optional('word_type'): word_type_insert_schema,
-    Optional('tags'): word_tags_insert_schema,
+    Optional('tags'): Schema([Use(str, error='tag error')]),
     Optional('sentences'): Schema([{
         'sentence': Use(str, error='sentences.sentence error'),
         'translation': Use(str, error='sentences.translation error')
